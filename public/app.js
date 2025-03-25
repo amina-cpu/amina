8d097152b03b50bc4123fb7237876bb095b30aad
amina-cpu <aminaotmanetelba@gmail.com>
2025-03-25 23:29:28 +0100

    Initial commit - responsive header added

--- public/app.js
+++ public/app.js
@@ -13,6 +13,25 @@ app.component('header-component', {
 
 </li></ul>
         </header>
+    <header class="header">
+  <div class="menu-icon" id="menu-icon">&#9776;</div>
+
+  <nav class="nav" id="nav">
+    <ul>
+      <li><a href="#i">About Me</a></li>
+      <li><a href="#skills">Skills</a></li>
+      <li><a href="#cont">My Work</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#tact">Contacts</a></li>
+      <li>
+        <a href="http://localhost:5000/cveng.pdf" target="_blank">
+          <button class="resume">Resumé</button>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
     `,
 });
 
